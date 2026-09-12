31476ae3f22a8168ae169e9ff481d1dbb5b9eef0
James Meowiarty <jamesmeowiarty@gmail.com>
2026-09-12 15:56:59 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # TTS_APP
+
+## Installing
+- Install Python
+    - https://www.python.org/
+    - Click Download and select your Operating System (NOTE: Latest version should work, but was tested with 3.14 and 3.12)
+    - Install Python (NOTE: For Windows you can use https://www.pythonguis.com/installation/install-tkinter-windows/ as a Guide)
+
+- Install Application Dependicies
+    - Open a Terminal and navigate to the Application directory
+    - Run "pip -r requirements.txt"
+    - Download a Voice into the Voices Folder
+        - Samples and Voices can be found at https://rhasspy.github.io/piper-samples/
+        - Click the "Download" Link on the chosen voice
+        - Download both the .onnx and .json files to the Voices directory in the application
+
+## Running the Application
+- Running the Application
+    - Open a Terminal and navigate to the Application directory
+    - run "python main.py"
+    - A GUI should pop up
+
+## Usage
+- Usage
+    - Choose an output device for the TTS to play through
+    - Choose a voice for the TTS
+    - Enter the text you wish to play
+    - Click "Play TTS"
+
+## Setting up a Live2D Model to mouth the TTS
+- Having VTube model "mouth" the TTS
+    - Recommended: Install VB-CABLE Virtual Audio Device
+        - https://vb-audio.com/Cable/
+        - This creates a fake Audio device which the application can interface with and the output can be added to VTube Studio and OBS
+    - Open VTube Studio
+        - Recommende: I recommend making a copy of the model before making changes. This way your have your normal mode and a TTS model
+        - Click "Settings"
+        - Scroll down to "Microphone Settings" and click "Use microphone
+        - Select "Microphone" and choose either your Microphone or "CABLE In 16 Ch (VB-Audio Virtua" if you installed VB-CABLE)"
+        - Click "Model Settings"
+        - Find the "Mouth Smile" setting (This may not be the exact name as the rigger can choose any name)
+        - Change the Input to "VoiceFrequency"
+        - Find the "Mouth Open" setting (This may not be the exact name as the rigger can choose any name)
+        - Change the Input to "VoiceVolume"
---
+++ main.py
@@ -0,0 +1,121 @@
+import pyaudio
+import string
+import tkinter as tk
+from tkinter import ttk
+
+from glob import glob
+from os import path
+from piper.voice import PiperVoice
+
+
+class TTS_APP():
+    def __init__(self):
+        self.available_models = [path.basename(x) for x in glob(path.join('Voices', '*.onnx'))]
+        self.voice = None
+        self.prev_voice = None
+        self.py_audio = pyaudio.PyAudio()
+
+    def get_voices(self):
+        return self.available_models
+
+    def play_tts(self, text, voice, audio_output_index):
+        if voice != self.voice:
+            self.voice = PiperVoice.load(path.join('Voices', voice))
+            self.prev_voice = voice
+        chunks = self.voice.synthesize(text)
+        first_chunk = next(chunks)
+
+        stream = self.py_audio.open( # Open audio stream with correct settings
+            format=self.py_audio.get_format_from_width(first_chunk.sample_width),
+            channels=first_chunk.sample_channels,
+            rate=first_chunk.sample_rate,
+            output=True,
+            output_device_index=audio_output_index
+        )
+
+        stream.write(first_chunk.audio_int16_bytes)
+
+        for chunk in chunks:
+            stream.write(chunk.audio_int16_bytes)
+
+        stream.stop_stream()
+        stream.close()
+
+
+tts_app = TTS_APP()
+
+def get_audio_devices():
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+
+    audio_devices = {}
+    for i in range(0, numdevices):
+        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            audio_devices[p.get_device_info_by_host_api_device_index(0, i).get('name')] = i
+    return audio_devices
+
+def play_tts():
+    text = text_text_box.get('1.0', 'end-1c')
+    tts_app.play_tts(text, combobox_piper_voice.get(), audio_outputs[combobox_audio_output.get()])
+    text_text_box.delete('1.0', tk.END)
+
+
+# Main window
+root = tk.Tk()
+root.title("TTS Player")
+root.geometry("500x250")
+
+# Make the second column expandable
+root.columnconfigure(1, weight=1)
+root.rowconfigure(2, weight=1)
+
+ttk.Label(root, text="Audio Outout").grid(
+    row=0, column=0, padx=10, pady=10, sticky="w"
+)
+
+audio_outputs = get_audio_devices()
+combobox_audio_output = ttk.Combobox(
+    root,
+    values=list(audio_outputs.keys()),
+    state="readonly"
+)
+combobox_audio_output.current(list(audio_outputs.keys()).index('default'))
+combobox_audio_output.grid(
+    row=0, column=1, padx=10, pady=10, sticky="ew"
+)
+
+ttk.Label(root, text="Piper TTS Voice").grid(
+    row=1, column=0, padx=10, pady=10, sticky="w"
+)
+
+piper_voices = tts_app.get_voices()
+combobox_piper_voice = ttk.Combobox(
+    root,
+    values=piper_voices,
+    state="readonly"
+)
+combobox_piper_voice.current(0)
+combobox_piper_voice.grid(
+    row=1, column=1, padx=10, pady=10, sticky="ew"
+)
+
+text_text_box = tk.Text(root, height=5, wrap="word")
+text_text_box.grid(
+    row=2, column=0, columnspan=2,
+    padx=10, pady=10,
+    sticky="nsew"
+)
+
+button_play = ttk.Button(
+    root,
+    text="Play TTS",
+    command=play_tts
+)
+button_play.grid(
+    row=3, column=1,
+    padx=10, pady=10,
+    sticky="e"
+)
+
+root.mainloop()
