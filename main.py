51be0c8af5ed40853ba5b01dad3bdbbcc1b839db
James Meowiarty <jamesmeowiarty@gmail.com>
2026-09-12 20:16:01 -0500

    Fixed issue getting output devices

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def get_audio_devices():
 
     audio_devices = {}
     for i in range(0, numdevices):
-        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        if (p.get_device_info_by_host_api_device_index(0, i).get('maxOutputChannels')) > 0:
             audio_devices[p.get_device_info_by_host_api_device_index(0, i).get('name')] = i
     return audio_devices
 
